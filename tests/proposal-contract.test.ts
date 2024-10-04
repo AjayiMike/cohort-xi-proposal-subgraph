ebc04bcae5d70903be1d6c26fcae3532b2d0ca13
AjayiMike <ajayimikeofficial@gmail.com>
2024-10-04 17:41:27 +0100

    Initial commit

---
+++ tests/proposal-contract.test.ts
@@ -0,0 +1,90 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { ProposalCreated } from "../generated/ProposalContract/ProposalContract"
+import { handleProposalCreated } from "../src/proposal-contract"
+import { createProposalCreatedEvent } from "./proposal-contract-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let proposalId = BigInt.fromI32(234)
+    let description = "Example string value"
+    let recipient = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let amount = BigInt.fromI32(234)
+    let votingDeadline = BigInt.fromI32(234)
+    let minVotesToPass = BigInt.fromI32(234)
+    let newProposalCreatedEvent = createProposalCreatedEvent(
+      proposalId,
+      description,
+      recipient,
+      amount,
+      votingDeadline,
+      minVotesToPass
+    )
+    handleProposalCreated(newProposalCreatedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "proposalId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "description",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "recipient",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "amount",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "votingDeadline",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "minVotesToPass",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
